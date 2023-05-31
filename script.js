d81084e6eadf6519bab31b5a00124383d45b4572
Abdimalik11 <ilkacaze5522@gmail.com>
2023-05-30 18:20:13 -0700

    this is the Answer of grades

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("hello");
+let grade=76
+if(grade>90&&grade<=100){
+    console.log("You got an A grade")
+}else if (grade<=90&&grade>75){
+    console.log("You got an B grade")
+}else if (grade<=75&& grade>60){
+    console.log("You got an c grade")
+}else if (grade<=60){
+    console.log("You got an FAILED")
+}else {
+    console.log("you are a looser")
+}
